9da848a39a0dba357d404a0b3c6e1030bdcd1f5e
JoseLuisTK <5joselbautistac5@gmail.com>
2026-07-24 18:08:48 -0600

    [FIX] entintados_pdv: quitar especificacion de modulo ya no necesaria en v19

--- static/src/js/tint_control_button.js
+++ static/src/js/tint_control_button.js
@@ -1,5 +1,3 @@
-/** @odoo-module **/
-
 import { patch } from "@web/core/utils/patch";
 import { _t } from "@web/core/l10n/translation";
 import { makeAwaitable } from "@point_of_sale/app/utils/make_awaitable_dialog";
