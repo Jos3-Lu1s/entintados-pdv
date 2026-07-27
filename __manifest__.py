7d4d4a33f6ec5953f7aeff993244e70333d57ff5
unknown <zayasd647@gmail.com>
2026-07-27 10:49:05 -0600

    Agregar riesgo fiscal

--- __manifest__.py
+++ __manifest__.py
@@ -19,7 +19,7 @@ Long description of module's purpose
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'point_of_sale', 'sale', 'sale_management', 'purchase'],
+    'depends': ['base', 'account', 'point_of_sale', 'sale', 'sale_management', 'purchase', 'contacts',],
 
     "assets": {
         "point_of_sale._assets_pos": [
@@ -29,12 +29,19 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/res_partner_views.xml',
         'views/sale_order_views.xml',
         'views/purchase_order_views.xml',
+        'views/account_financial_risk_view.xml',
+        'views/portal_templates.xml',
+        'views/res_config_risk_view.xml',
+        'views/res_partner_risk_view.xml',
+        "wizards/partner_risk_exceeded_view.xml",
+
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,7 @@
 
 from . import models
 from . import res_partner
-
+from . import account_innvoice_risk
+from . import res_company_risk
+from . import res_config_risk
+from . import res_partner_risk
